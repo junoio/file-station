29bf5b6e93e6721a1107fdaba98f2ba0ef62489c
冯恒俊 <876100295@qq.com>
2023-08-21 01:46:18 +0800

    打印下载url

--- main.go
+++ main.go
@@ -1,10 +1,79 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"os"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
-func main() {
+var client *oss.Client
+var (
+	Api             = "https://oss-cn-beijing.aliyuncs.com"
+	AccessKeyId     = ""
+	AccessKeySecret = ""
+	Bucket          = "f-devclould-station"
+	Path            = ""
+)
 
-	fmt.Println(1)
+func init() {
+	var err error
+	client, err = oss.New(Api, AccessKeyId, AccessKeySecret)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "连接失败："+err.Error())
+		os.Exit(1)
+	}
+}
+
+func uploadFile(path string) (string, error) {
+	bucket, err := client.Bucket(Bucket)
+	if err != nil {
+		return "", err
+	}
+	err = bucket.PutObjectFromFile(path, path)
+	if err != nil {
+		return "", err
+	}
+	//打印下载链接
+	return bucket.SignURL(path, oss.HTTPGet, 60*60)
+
+}
+
+func validParams() error {
+	if Api == "" || AccessKeyId == "" || AccessKeySecret == "" || Bucket == "" {
+		panic("参数Api,AccessKeyId,AccessKeySecret,Bucket未设置")
+	}
+
+	if Path == "" {
+		return errors.New("please see -h")
+	}
+	return nil
+}
+
+func loadParam() {
+	//覆盖usage
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage : command -f filepath \nOptions:")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&Path, "f", "", "-f filepath")
+	flag.Parse()
+}
+
+func main() {
+	loadParam()
+	err := validParams()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "usage error:", err.Error())
+		os.Exit(1)
+	}
+	s, err := uploadFile(Path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "上传失败：", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("文件上传成功！\n 下载链接：%s\n下载链接有效时间1小时。", s)
+
 }
